dc60f9ba7d891bd299c49845a2ad028cbcdb6e61
Aleksei <lexa24032@gmail.com>
2022-06-22 12:48:52 +0300

    buttons are unclickable, will be fixed in nearest future

--- app/js/chunks/_filter.js
+++ app/js/chunks/_filter.js
@@ -2,114 +2,168 @@ const gridItems = [
   {
     title: 'koz 01',
     category: 'koz',
-    img: '/forge-temp/app/img/grid/koz/koz-01.jpg',
+    img: 'img/grid/koz/koz-01.webp',
   },
   {
     title: 'koz 02',
     category: 'koz',
-    img: '/forge-temp/app/img/grid/koz/koz-02.jpg',
+    img: '../img/grid/koz/koz-02.webp',
   },
   {
     title: 'naves 01',
     category: 'naves',
-    img: '/forge-temp/app/img/grid/naves/n-01.jpg',
+    img: '../img/grid/naves/n-01.webp',
   },
   {
     title: 'naves 02',
     category: 'naves',
-    img: '/forge-temp/app/img/grid/naves/n-02.jpg',
+    img: '../img/grid/naves/n-02.webp',
   },
   {
     title: 'besed 01',
     category: 'besed',
-    img: '/forge-temp/app/img/grid/besed/b-01.jpg',
+    img: '../img/grid/besed/b-01.webp',
   },
   {
     title: 'besed 02',
     category: 'besed',
-    img: '/forge-temp/app/img/grid/besed/b-02.jpg',
+    img: '../img/grid/besed/b-02.webp',
   },
   {
     title: 'gate 01',
     category: 'gate',
-    img: '/forge-temp/app/img/grid/gate/g-01.jpg',
+    img: '../img/grid/gate/g-01.webp',
   },
   {
     title: 'gate 02',
     category: 'gate',
-    img: '/forge-temp/app/img/grid/gate/g-02.jpg',
+    img: '../img/grid/gate/g-02.webp',
   },
   {
     title: 'resh 01',
     category: 'resh',
-    img: '/forge-temp/app/img/grid/resh/r-01.jpg',
+    img: '../img/grid/resh/r-01.webp',
   },
   {
     title: 'resh 02',
     category: 'resh',
-    img: '/forge-temp/app/img/grid/resh/r-02.jpg',
+    img: '../img/grid/resh/r-02.webp',
   },
   {
     title: 'stare 01',
     category: 'stare',
-    img: '/forge-temp/app/img/grid/stare/stare-01.jpg',
+    img: '../img/grid/stare/stare-01.webp',
   },
   {
     title: 'stare 02',
     category: 'stare',
-    img: '/forge-temp/app/img/grid/stare/stare-02.jpg',
+    img: '../img/grid/stare/stare-02.webp',
   },
   {
     title: 'mang 01',
     category: 'mang',
-    img: '/forge-temp/app/img/grid/mang/m-01.jpg',
+    img: '../img/grid/mang/m-01.webp',
   },
   {
     title: 'mang 02',
     category: 'mang',
-    img: '/forge-temp/app/img/grid/mang/m-02.jpg',
+    img: '../img/grid/mang/m-02.webp',
   },
   {
     title: 'peril 01',
     category: 'peril',
-    img: '/forge-temp/app/img/grid/peril/peril-01.jpg',
+    img: '../img/grid/peril/peril-01.webp',
   },
   {
     title: 'peril 02',
     category: 'peril',
-    img: '/forge-temp/app/img/grid/peril/peril-02.jpg',
+    img: '../img/grid/peril/peril-02.webp',
   },
   {
     title: 'init 01',
     category: 'init',
-    img: '/forge-temp/app/img/grid/init/i-01.jpg',
+    img: '../img/grid/init/i-01.webp',
   },
   {
     title: 'init 02',
     category: 'init',
-    img: '/forge-temp/app/img/grid/init/i-02.jpg',
+    img: '../img/grid/init/i-02.webp',
   },
   {
     title: 'ogra 01',
     category: 'ogra',
-    img: '/forge-temp/app/img/grid/ogra/o-01.jpg',
+    img: '../img/grid/ogra/o-01.webp',
   },
   {
     title: 'ogra 02',
     category: 'ogra',
-    img: '/forge-temp/app/img/grid/ogra/o-02.jpg',
+    img: '../img/grid/ogra/o-02.webp',
   },
   {
     title: 'other 01',
     category: 'other',
-    img: '/forge-temp/app/img/grid/other/oth-01.jpg',
+    img: '../img/grid/other/oth-01.webp',
   },
   {
     title: 'other 02',
     category: 'other',
-    img: '/forge-temp/app/img/grid/other/oth-02.jpg',
+    img: '../img/grid/other/oth-02.webp',
   },
 ];
 
 const displayCatalogue = document.querySelector('.display-catalogue');
 const btnsCatalogue = document.querySelector('.buttons-catalogue');
+
+window.addEventListener('DOMContentLoaded', () => {
+  displayGridCatalogue(gridItems);
+  displayChoiceButtons();
+});
+
+function displayGridCatalogue(gridItems) {
+  let display = gridItems.map((item) => {
+    return `
+    <article class="display-catalogue__item item-catalogue">
+      <div class="item-catalogue__img">
+        <img src="${item.img}" alt="${item.title}" />
+      </div>
+    </article>
+    `;
+  });
+
+  display = display.join('');
+  displayCatalogue.innerHTML = display;
+}
+
+function displayChoiceButtons() {
+  const gridCategories = gridItems.reduce(
+    (values, item) => {
+      if (!values.includes(item.category)) {
+        values.push(item.category);
+      }
+      return values;
+    },
+    ['all']
+  );
+
+  const categoryButtons = gridCategories
+    .map((category) => {
+      return `
+      <button type="button" title="choose" data-category="${category}" class="buttons-catalogue__choice">${category}</button>
+    `;
+    })
+    .join('');
+
+  btnsCatalogue.innerHTML = categoryButtons;
+
+  const chooseButtons = btnsCatalogue.querySelectorAll('.buttons-catalogue__choice');
+  chooseButtons.forEach((elem) => {
+    elem.addEventListener('click', (e) => {
+      const itemsCategory = e.currentTarget.dataset.category;
+      const buttonsCategory = gridItems.filter((item) => {
+        if (item.category === itemsCategory) {
+          return item;
+        }
+      });
+    });
+  });
+}
